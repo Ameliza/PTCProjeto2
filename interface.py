141bbdb24b1705e619b72edaab658aeab691f065
camillabarreto <camillabarretodesousa@gmail.com>
2021-04-04 14:58:14 -0300

    menu interface

--- interface.py
+++ interface.py
@@ -2,14 +2,51 @@
 
 from cliente import Cliente
 
-if __name__ == '__main__':
+LOGIN = '1'
+PROVA = '2'
+RESPOSTAS = '3'
+RESULTADO = '4'
+LOGOUT = '5'
 
-    print("*** LOGIN ***\n")
+def menu(switch):
+    
+    print("------ MENU ------")
+    print("1 - Login")
+    print("2 - Requisição de prova")
+    print("3 - Respostas da prova")
+    print("4 - Resultado da prova")
+    print("5 - Logout")
+    option = input(">> ")
+    switch[option]()
+
+def login():
+    print("****** LOGIN *****\n")
     login = input("Login: ")
     senha = input("Senha: ")
-
-    print("O usuário informado foi: %s, e a senha digitada foi: %s" %(login, senha))
-
     cliente = Cliente(login, senha)
-    cliente.authenticate()
+    cliente.authenticate() 
+
+def prova():
+    print("prova")
+
+def respostas():
+    print("respostas")
+
+def resultado():
+    print("resultado")
+
+def logout():
+    print("logout")
+
+if __name__ == '__main__':
+    cliente = None
+    switch = {
+        LOGIN: login,
+        PROVA: prova,
+        RESPOSTAS: respostas,
+        RESULTADO: resultado,
+        LOGOUT: logout
+    }
+
+    menu(switch)
     
