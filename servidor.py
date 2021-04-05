1ca9767b1551a88014bbdef6ddb0061050f28882
camillabarreto <camillabarretodesousa@gmail.com>
2021-04-04 22:39:33 -0300

    organizado servidor

--- servidor.py
+++ servidor.py
@@ -10,7 +10,7 @@ senha = ["aluno"]
 token = None
 
 def checklogin(msg):
-    
+    print("LOGIN")
     # verifica se pode autenticar ...
     if msg.login == usuario[0] and msg.senha == senha[0]:
         global token
@@ -19,28 +19,46 @@ def checklogin(msg):
     else:
         return API.acklogin('000000000')
 
+def reqprova():
+    print("REQPROVA")
+
+def reqresp():
+    print("REQRESP")
+
+def reqresultado():
+    print("REQRESULTADO")
+
 def logout(msg):
+    print("LOGOUT")
     global token
     token = None
 
 if __name__ == '__main__':
+    
     s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
     s.bind(('0.0.0.0', 8888))
     s.listen()  # espera conexões na porta
+
     while True:
         print('Esperando conexão...')
         con, addr = s.accept()
-    
         data = con.recv(1024)
-        print("conectou")
 
         msg,des = API.mensagem(data)
         if des=='login': # se for mensagem de login
-            print("LOGIN")
             data = checklogin(msg)
             con.send(data)
+
+        elif des=='reqprova':
+            reqprova()
+            
+        elif des=='reqresp':
+            reqresp()
+            
+        elif des=='reqresultado':
+            reqresultado()
+            
         elif des=='logout':
-            print("LOGOUT")
             logout(msg)
 
         print("token: ", token)
